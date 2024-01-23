1c177853d34783135b8a7f1da1737106f90d0f27
alpha-tango-kilo <git@heyatk.com>
2024-01-23 19:08:24 +0000

    Massively over-engineer progress formatting

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,10 @@ use std::{
     time::Duration,
 };
 
-const ONE_SECOND: Duration = Duration::from_secs(1);
+const ONE_DAY_SECONDS: u64 = 60 * 60 * 24;
+const ONE_HOUR_SECONDS: u64 = 60 * 60;
+const ONE_MINUTE_SECONDS: u64 = 60;
+
 const SPINNER: &[char] = &['|', '/', '-', '\\'];
 
 fn main() {
@@ -30,18 +33,19 @@ fn idler(mut args: impl Iterator<Item = String>) -> Result<(), Box<dyn Error>> {
     };
     let mut stdout = stdout();
     if stdout.is_terminal() {
-        let seconds = duration.as_secs();
-        for current in 0..seconds {
+        let total_seconds = duration.as_secs();
+        let duration = HumanDuration::from(duration);
+        for current in 0..total_seconds {
             let spinner_char =
                 SPINNER[(current % (SPINNER.len() as u64)) as usize];
             let message = format!(
-                "\rIdling {app_id} for {duration:?}: {:?}, {}% {spinner_char} ",
-                Duration::from_secs(current),
-                69,
+                "\rIdling {app_id} for {duration}: {}, {:.1}% {spinner_char} ",
+                HumanDuration::from_secs(current),
+                current as f32 / total_seconds as f32 * 100f32,
             );
             stdout.write_all(message.as_bytes()).unwrap();
             stdout.flush().unwrap();
-            sleep(ONE_SECOND);
+            sleep(Duration::from_secs(1));
         }
         println!();
     } else {
@@ -65,9 +69,9 @@ fn parse_duration(
                     // characters are ASCII digits
                     let value = input[slice_start..i].parse::<u64>().unwrap();
                     let scale = match c {
-                        'd' => 60 * 60 * 24,
-                        'h' => 60 * 60,
-                        'm' => 60,
+                        'd' => ONE_DAY_SECONDS,
+                        'h' => ONE_HOUR_SECONDS,
+                        'm' => ONE_MINUTE_SECONDS,
                         's' => 1,
                         _ => unreachable!(),
                     };
@@ -106,6 +110,67 @@ impl fmt::Display for ParseDurationError {
 }
 
 impl Error for ParseDurationError {}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct HumanDuration {
+    days: u64,
+    hours: u8,
+    minutes: u8,
+    seconds: u8,
+}
+
+impl HumanDuration {
+    const ZERO: Self = HumanDuration {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+    };
+
+    fn from_secs(secs: u64) -> Self {
+        let mut remaining_seconds = secs;
+        let days = remaining_seconds / ONE_DAY_SECONDS;
+        remaining_seconds %= ONE_DAY_SECONDS;
+        let hours = (remaining_seconds / ONE_HOUR_SECONDS) as u8;
+        remaining_seconds %= ONE_HOUR_SECONDS;
+        let minutes = (remaining_seconds / ONE_MINUTE_SECONDS) as u8;
+        remaining_seconds %= ONE_MINUTE_SECONDS;
+        let seconds = remaining_seconds as u8;
+        HumanDuration {
+            days,
+            hours,
+            minutes,
+            seconds,
+        }
+    }
+}
+
+impl From<Duration> for HumanDuration {
+    fn from(value: Duration) -> Self {
+        HumanDuration::from_secs(value.as_secs())
+    }
+}
+
+impl fmt::Display for HumanDuration {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if *self == HumanDuration::ZERO {
+            return write!(f, "0s");
+        }
+        if self.days != 0 {
+            write!(f, "{}d", self.days)?;
+        }
+        if self.hours != 0 {
+            write!(f, "{}h", self.hours)?;
+        }
+        if self.minutes != 0 {
+            write!(f, "{}m", self.minutes)?;
+        }
+        if self.seconds != 0 {
+            write!(f, "{}s", self.seconds)?;
+        }
+        Ok(())
+    }
+}
 
 #[cfg(test)]
 mod tests {
